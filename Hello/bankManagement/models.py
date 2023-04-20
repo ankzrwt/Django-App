2443c63016fa1d1db4d126dfb06e0e2700bdb144
ankzrwt <rawat.ankita1998@gmail.com>
2023-04-20 22:53:55 +0530

    Initial commit

---
+++ Hello/bankManagement/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    customerId=models.CharField(max_length=20)
+    phone=models.IntegerField()
+    name=models.CharField(max_length=30)
+
+class Account(models.Model):
+    accountId=models.CharField(max_length=13)
+    customerId=models.CharField(max_length=13)
+    balance=models.IntegerField(0)
+
+
